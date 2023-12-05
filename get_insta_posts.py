28e1cc6ffc8f57ec392946282e89b0755c678a85
Bence Bánsághi <35151749+bencebansaghi@users.noreply.github.com>
2023-12-05 16:28:02 +0200

    Update get_insta_posts.py
    Cleaned up code, with all instagram handles included in main, turned on quiet

--- get_insta_posts.py
+++ get_insta_posts.py
@@ -1,22 +1,23 @@
 from instaloader import Instaloader, Profile
 from datetime import datetime, timedelta
+from itertools import dropwhile, takewhile
 
 def return_captions(profiles):
-    L = Instaloader()
+    L = Instaloader(quiet=True)
     captions = []
     for username in profiles:
-        profile = Profile.from_username(L.context, username)
-        posts = sorted(profile.get_posts(), key=lambda post: post.likes, reverse=True)
-        
+        posts = Profile.from_username(L.context, username).get_posts() 
         now = datetime.now()
         one_week_ago = now - timedelta(weeks=1)
-
-        selected_range = [post for post in posts if one_week_ago <= post.date <= now]
-        for post in selected_range:
+        for post in takewhile(lambda p: p.date > one_week_ago, dropwhile(lambda p: p.date > now, posts)):
             L.download_post(post, username)
             captions.append(post.caption)
 
     return captions
         
 if __name__ == "__main__":
-    print(return_captions(["lahoevents","aether_ry"]))
+    profiles = ["aether_ry", "lahoevents", "koeputkiappro", "aleksinappro", "lasolary", "lymo.ry", "lirory", "Moveolahti", "koe_opku", "linkkiry"]
+    captions = return_captions(profiles)
+    for caption in captions:
+        print(caption)
+
