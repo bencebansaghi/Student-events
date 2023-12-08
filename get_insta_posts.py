724471d80e4e3bea956f6d6eb675d11b9f72620b
Bence Bánsághi <35151749+bencebansaghi@users.noreply.github.com>
2023-12-08 23:37:59 +0200

    updated gitignore, fixed current dir

--- get_insta_posts.py
+++ get_insta_posts.py
@@ -3,40 +3,42 @@ from instaloader.instaloader import Instaloader
 from instaloader.structures import Profile
 from datetime import datetime, timedelta
 from itertools import dropwhile, takewhile
-from dotenv import load_dotenv
 import instaloader.exceptions
-from requests import session
-import os
+import pathlib
 
 
 
-def return_captions(profiles,session_file):
-    
-
-    with Instaloader(quiet=True) as L:
+def return_captions(profiles, session_file):
+    with Instaloader(quiet=True,dirname_pattern=".\\profiles") as L:
         try:
-            L.load_session_from_file("bencebansaghi",filename=session_file)
+            L.load_session_from_file("bencebansaghi", filename=session_file)
         except instaloader.exceptions.ConnectionException as e:
             print(f"Login error: {e}")
+        except FileNotFoundError as e:
+            print(f"File not found error: {e}\nPlease make sure the session is saved to the same dir as the script.")
         except Exception as e:
             print(f"Unknown error: {e}")
 
     captions = []
-    for usernames in profiles:
-        posts = Profile.from_username(L.context, usernames).get_posts() 
+    for username in profiles:
+        try:
+            posts = Profile.from_username(L.context, username).get_posts() 
+        except instaloader.exceptions.ProfileNotExistsException as e:
+            print(f"Profile {username} does not exist: {e}")
+            continue
         now = datetime.now()
         one_week_ago = now - timedelta(weeks=1)
         for post in takewhile(lambda p: p.date > one_week_ago, dropwhile(lambda p: p.date > now, posts)):
-            L.download_post(post, usernames)
+            L.download_post(post, username)
             captions.append(post.caption)
 
     return captions
 
 if __name__ == "__main__":
     profiles = ["aether_ry", "lahoevents", "koeputkiappro", "aleksinappro", "lasolary", "lymo.ry", "lirory", "Moveolahti", "koe_opku", "linkkiry"]
-    session_file_path = os.getcwd()
-    session_file_name = "\\session-bencebansaghi"
-    session_file = session_file_path + session_file_name
+    session_file_path = str(pathlib.Path(__file__).parent.resolve()) # Get the path of the script
+    session_file_name = "\\session-bencebansaghi" # Name of the session file
+    session_file = session_file_path + session_file_name # Full path of the session file
     captions = return_captions(profiles,session_file)
     for caption in captions:
         print(caption)
