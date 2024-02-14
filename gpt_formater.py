e33d9299f0618bb9fc25d96281999e659ff38aa0
Bence Bánsághi <35151749+bencebansaghi@users.noreply.github.com>
2024-02-14 17:57:17 +0200

    added post link
    still needs proper error handling

--- gpt_formater.py
+++ gpt_formater.py
@@ -1,4 +1,6 @@
+import json
 import os
+import re
 from dotenv import load_dotenv
 from openai import OpenAI
 from get_insta_posts import return_captions
@@ -6,12 +8,12 @@ import api_key
 
 def return_formated_events(profiles,session_file_path,session_file_name):
     api_key_value=None
-    try: #first we try to get API key from api_key.py
+    try: #first we try to get API key from .env variables
         load_dotenv()
-        api_key_value = str(os.getenv("OPENAI_API_KEY")) #casting to string is necessary because of the way dotenv works
-        print("Got API key from .env variables")
+        api_key_value = str(os.getenv("API_KEY")) #casting to string is necessary because of the way dotenv works
         if api_key_value is None:
             raise Exception("API key not found in .env variables")
+        print("Got API key from .env variables")
     except Exception as e:
         api_key_value = api_key.get_api_key() # Get the API key from api_key.py
         print("Got API key from api_key.py")
@@ -35,30 +37,40 @@ def return_formated_events(profiles,session_file_path,session_file_name):
         print("No captions found")
         return None
     
+
+    
     # Create a stream to process the captions using the OpenAI API
-    try:
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[{
-                "role": "user", 
-                "content": f'data: {str(captions)}. Your job is to identify and extract events from the data and return their data in the following format: [{{"date" - date of the event, format %d.%m.%Y}},{{"name" - name of the event}},{{"description" - short description of the event (maximum 6 sentences)}}]. Please translate both the event names and descriptions into English. If an event lacks a specific date, skip it.'
-            }],
+    gptd_post_array = []
+    for post in captions:
+        try:
+            stream = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[{
+                    "role": "user", 
+                    "content": f'data: {post["caption"]}. Your job is to check if the data contains an event. If it does not contain an event, return python None. If it does contain an event, return its data in the following format: {{"date" : date of the event, format %d.%m.%Y , "name" : name of the event,"description" : short description of the event (maximum 6 sentences)}}. Translate both the event names and descriptions into English, if it is not already in English. If an event lacks a specific date, skip it, and return python None.'
+                }],
+                
+                stream=True,
+            )
             
-            stream=True,
-        )
-    except Exception as e:
-        print(f"Error while creating stream: {e}")
-        return None
+            # Process and append the output from the stream to an array
+            output_array = []
+            for chunk in stream:
+                if chunk.choices[0].delta.content is not None:
+                    output_array.append(chunk.choices[0].delta.content)
+        except Exception as e:
+            print(f"Error while creating stream: {e}")
+            return None
+        combined_output = ''.join(output_array)
+        if combined_output is None or combined_output == "" or combined_output == "None":
+            continue
+        combined_output_dict=json.loads(combined_output)
+        combined_output_dict["link"]=post["link"]
+        gptd_post_array.append(combined_output_dict)
 
-    # Process and append the output from the stream to an array
-    output_array = []
-    for chunk in stream:
-        if chunk.choices[0].delta.content is not None:
-            output_array.append(chunk.choices[0].delta.content)
 
-    combined_output = ''.join(output_array)
-
-    return combined_output
+    return gptd_post_array
+    
 
     
 if __name__ == "__main__":
